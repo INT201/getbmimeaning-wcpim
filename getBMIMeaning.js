fe73190c06c6adaca08c7dc9d4de1a62bfe54c70
wcpimpim <waristha.0204@mail.kmutt.ac.th>
2022-09-04 02:11:31 +0700

    bmi

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,22 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+ BMI = weight/(height^2)
+ return BMI
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  calculateBMI(weight, height)
+  if (BMI<18.5) {
+    return "Underweight"
+  }
+  else if (BMI<=24.9){
+    if(BMI>=18.5){
+      return "Normal weight"
+    }
+  }
+  else (BMI>25.0){
+    return "Overweight"
+  }
 }
+
 module.exports = getBMIMeaning
